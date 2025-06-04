c49fa1b0a539efdbc47a776d434a1e4f853c8668
K4ues <124373847+K4ues@users.noreply.github.com>
2025-06-04 17:03:48 +0000

    Update final 1.1

--- tailwind-spotify/src/components/Footer.tsx
+++ tailwind-spotify/src/components/Footer.tsx
@@ -16,7 +16,7 @@ export function Footer() {
             <Shuffle size={20} className="text-zinc-200" />
             <SkipBack size={20} className="text-zinc-200" /> 
 
-            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white-400 text-black">
+            <button className="w-10 h-10 flex items-center justify-center pl-1 rounded-full bg-white text-black">
               <Play />
             </button>
             <SkipForward size={20} className="text-zinc-200" />
